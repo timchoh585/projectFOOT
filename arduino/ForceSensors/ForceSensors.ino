a76a576c1eaf36e1f0215453c00b8caa8e7bc688
Tim Choh <timchoh@gmail.com>
2018-04-17 14:46:22 -0500

    formatting arduino code

--- arduino/ForceSensors/ForceSensors.ino
+++ arduino/ForceSensors/ForceSensors.ino
@@ -1,47 +1,30 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BTserial(3, 2); // RX | TX
-
 int forceSensor = 0;
-
 int forceReading;
-
 char startString = '#';
 
 char endString = '~';int LEDPin = 13;void setup() {
+  BTserial.begin(9600);  
+  Serial.begin(9600);
+  pinMode(LEDPin,OUTPUT);
+}
 
+void sendForceData() {
+  forceReading = analogRead(forceSensor);
+  //Serial.print(forceReading);
+  int mappedValue = forceReading;//map(forceReading, 0, 767, 0000, 1023);
+  char myBuffer[30];
+  sprintf(myBuffer,"%c%04d%04d%04d%04d%04d%04d%04d%c", startString, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, endString );  BTserial.println(myBuffer);
+  Serial.println(myBuffer);
+}
  
-
- BTserial.begin(9600);  
-
- Serial.begin(9600);
-
- pinMode(LEDPin,OUTPUT);
-
-}void sendForceData() {
-
- forceReading = analogRead(forceSensor);
-
- //Serial.print(forceReading);
-
- int mappedValue = forceReading;//map(forceReading, 0, 767, 0000, 1023);
-
- char myBuffer[30];
-
- sprintf(myBuffer,"%c%04d%04d%04d%04d%04d%04d%04d%c", startString, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, mappedValue, endString );  BTserial.println(myBuffer);
-
- Serial.println(myBuffer);}void loop ()
-
+void loop ()
 {
-
- if(BTserial.available())
-
- {
-
-   sendForceData();
-
- }
-
- delay(100);
-
+  if(BTserial.available())
+  {
+    sendForceData();
+  }
+  delay(100);
 }
