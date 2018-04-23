336786f4a2ad6fea1e770ffd44db155db34fb6e2
Tim Choh <timchoh@gmail.com>
2018-04-23 14:45:46 -0500

    working through it

--- projectFOOT_android/app/src/main/java/foot/project/projectfoot/MainActivity.java
+++ projectFOOT_android/app/src/main/java/foot/project/projectfoot/MainActivity.java
@@ -197,7 +197,7 @@ public class MainActivity extends Activity {
 
         editor.apply();
     }
-    
+
 
 
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
