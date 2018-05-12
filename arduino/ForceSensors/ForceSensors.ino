a8841e6e24423ee77e9eccbabafeb086d873b8d4
Ryan Nishimoto <nishimo1@uic.edu>
2018-05-12 17:41:40 -0500

    Final code

--- arduino/ForceSensors/ForceSensors.ino
+++ arduino/ForceSensors/ForceSensors.ino
@@ -1,13 +1,13 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTserial(3, 2); 
 // RX | TX on Arduino (so HC-05 RX goes to pin 2, TX goes to pin 3, GND goes between 1 and 2k resistors)
-int forceSensor0 = 0;
-int forceSensor1 = 2;
-int forceSensor2 = 3;
-int forceSensor3 = 4;
-int forceSensor4 = 5;
-int forceSensor5 = 6;
-int forceSensor6 = 1;
+int forceSensor0 = 0; // Toe
+int forceSensor1 = 1; // Ball
+int forceSensor2 = 2; // Side 
+int forceSensor3 = 3; // Side
+int forceSensor4 = 4; // Side
+int forceSensor5 = 5; // Side
+int forceSensor6 = 6; // Heel
 
 int forceSensor[7] = { 0, 0, 0, 0, 0, 0, 0 };
 
@@ -19,7 +19,7 @@ bool startRec = false;
 bool stopRec = false;
 int toe = 0;
 int heel = 6;
-int threshold = 130;
+int threshold = 150;
 
 void setup() {
   
@@ -32,11 +32,11 @@ void testSensor() {
   int heelValue = forceReading6;
 
   forceSensor[0] = forceReading0;
-  forceSensor[1] = 0;//forceReading1;
-  forceSensor[2] = 0;//forceReading2;
-  forceSensor[3] = 0;//forceReading3;
-  forceSensor[4] = 0;//forceReading4;
-  forceSensor[5] = 0;//forceReading5;
+  forceSensor[1] = forceReading1;
+  forceSensor[2] = forceReading2;
+  forceSensor[3] = forceReading3;
+  forceSensor[4] = forceReading4;
+  forceSensor[5] = forceReading5;
   forceSensor[6] = forceReading6;
 }
 
@@ -66,6 +66,14 @@ bool greaterThanRestOfSensors( int sensor ) {
   return true;
 }
 
+bool allUnderThreshold(){
+  for (int i = 0; i < 6; i++){
+    if (forceSensor[i] > threshold){
+      return false;
+    }
+  }
+  return true;
+}
 void checkForStep() {
   if( greaterThanRestOfSensors( heel ) && !stopRec) {
     if(forceSensor[ heel ] > threshold){
@@ -73,7 +81,7 @@ void checkForStep() {
       startRec = true;
     }
   }
-  if( greaterThanRestOfSensors( toe )  && startRec) {
+  if(startRec) {
       stopRec = true;
     if ((forceSensor[ toe ] < threshold) && (forceSensor[ heel ] < threshold) && stepping){
       char endBuffer[31];
@@ -85,14 +93,22 @@ void checkForStep() {
       startRec= false;
     }
   }
+  /*
   // Starting step with toe
-  if( greaterThanRestOfSensors( toe )  && !startRec ){
+  if( greaterThanRestOfSensors( toe )  && !startRec && forceSensor[toe] > threshold){
     char errorBuffer[3];
-    sprintf(errorBuffer,"%03c",startString,'^',endString);
+    sprintf(errorBuffer,"%c%c%c",startString,'^',endString);
     BTserial.print(errorBuffer);
     Serial.println(errorBuffer);
     
-  }
+  }*/
+}
+
+void calibrate(){
+  char myBuffer[34];
+  char space = ' ';
+  sprintf(myBuffer,"%04d%c%04d%c%04d%c%04d%c%04d%c%04d%c%04d", forceSensor[0], space, forceSensor[1], space, forceSensor[2], space, forceSensor[3], space, forceSensor[4], space, forceSensor[5], space, forceSensor[6]);
+  Serial.println(myBuffer);
 }
 
 void loop ()
@@ -105,15 +121,18 @@ void loop ()
   forceReading4 = analogRead(forceSensor4);
   forceReading5 = analogRead(forceSensor5);
   forceReading6 = analogRead(forceSensor6);
-  testSensor();
-  
+  testSensor();  
   checkForStep();
-  //if(BTserial.available() && stepping )
-  //{
-  if(stepping){
+  // Send data over bluetooth if stepping and connected to bluetooth
+  if(BTserial.available() && stepping ){
     sendForceData();
   }
-  //}
+  // Otherwise print to serial the sensor information for debugging
+  else{
+    calibrate();
+  }
+  
+  
   
   delay(333);
 }
